8723583a11dc9da5d822a293690f21e425801faa
binkul <jacekbinkul@gmail.com>
2020-02-16 19:30:06 +0100

    Cleaning

--- src/main/java/com/rest/send/controller/FileNotSavedException.java
+++
@@ -1,9 +0,0 @@
-package com.rest.send.controller;
-
-import java.io.IOException;
-
-public class FileNotSavedException extends IOException {
-    public FileNotSavedException(String message) {
-        super(message);
-    }
-}
